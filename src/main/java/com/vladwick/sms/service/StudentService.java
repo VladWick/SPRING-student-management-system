a58ceadcc5d935e3f7b9fc8b6b59682b35803dff
VladWick <91905024+VladWick@users.noreply.github.com>
2022-04-05 02:54:47 +0700

    Initial commit

---
+++ src/main/java/com/vladwick/sms/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.vladwick.sms.service;
+
+import java.util.List;
+
+import com.vladwick.sms.entity.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student updateStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	void deleteStudentById(Long id);
+}
